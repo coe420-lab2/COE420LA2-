0177e0d4d96f9fa7b7c719379c9431e24607ea3a
Touqa Beltagui <g00069021@aus.edu>
2019-02-10 16:54:17 +0400

    Feature B added

--- src/coe420la2/COE420LA2.java
+++ src/coe420la2/COE420LA2.java
@@ -14,6 +14,10 @@ public class COE420LA2 {
     {
         System.out.println("Feature A added");
     }
+    public static void addFeatureB()
+    {
+        System.out.println("Feature B added");
+    }
 
     /**
      * @param args the command line arguments
@@ -21,6 +25,7 @@ public class COE420LA2 {
     public static void main(String[] args) {
         // TODO code application logic here
         addFeatureA();
+        addFeatureB();
     }
     
 }
